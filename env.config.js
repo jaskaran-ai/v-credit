8c88ba58c211df1ffc7453136f5af976544ff999
Macbook air M2 <macbookairm2@Macbooks-MacBook-Air.local>
2025-12-11 10:51:16 +0530

    url update

--- env.config.js
+++ env.config.js
@@ -1,15 +1,15 @@
 export const ENV = {
-  DEBUG: true,
-  DEV: {
-    BASE_URL: 'https://dev.mycreditbook.com/api/v1/',
-    IMAGE_URL: 'https://dev.mycreditbook.com/public/images/',
-  },
-  PRO: {
-    BASE_URL: 'http://13.203.200.232/api/v1/',
-    IMAGE_URL: 'http://13.203.200.232/public/images/',
-  },
-  LOCAL: {
-    BASE_URL: 'http://127.0.0.1:8000/api/v1/',
-    IMAGE_URL: 'http://127.0.0.1:8000/public/images/',
-  },
+	DEBUG: false,
+	DEV: {
+		BASE_URL: 'https://mycreditbook.com/api/v1/',
+		IMAGE_URL: 'https://mycreditbook.com/public/images/',
+	},
+	PRO: {
+		BASE_URL: 'https://mycreditbook.com/api/v1/',
+		IMAGE_URL: 'https://mycreditbook.com/public/images/',
+	},
+	LOCAL: {
+		BASE_URL: 'https://mycreditbook.com/api/v1/',
+		IMAGE_URL: 'https://mycreditbook.com/public/images/',
+	},
 };
